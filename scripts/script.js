ed317b0d0bad7eeea8757142b5d262c97c3aadb6
lottekoblens <74048067+lottekoblens@users.noreply.github.com>
2020-11-24 17:18:03 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+var deButton = document.querySelector("button");
+deButton.addEventListener("click", navMenu);
+
+function navMenu() {
+    var navSelect = document.querySelector("header nav");
+    navSelect.classList.toggle("nav.toonNav");
+}
+
+console.log('hoi');
