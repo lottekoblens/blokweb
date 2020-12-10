109c11055de6568b5a39d4c1a356c639e3f2fe8f
lottekoblens <74048067+lottekoblens@users.noreply.github.com>
2020-12-10 15:38:44 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,26 @@
 // JavaScript Document
+
+// navigatie menu
 var deButton = document.querySelector("nav button");
 deButton.addEventListener("click", linksEnRechts);
 
 function linksEnRechts(event) {
     navSelect = event.target.parentNode;
     navSelect.classList.toggle("toonNav");
+}
+// einde navigatie menu
+
+// image slider
+var pijlButton = document.querySelector("#homepagina > ul:nth-of-type(1) button");
+pijlButton.addEventListener("click", pijlGroter);
+
+function pijlGroter() {
+    pijlButton.classList.add("klikt");
+    window.setTimeout(remove, 500);
+}
+
+function remove() {
+    pijlButton.classList.remove("klikt");
 }
 
 console.log('hoi');
